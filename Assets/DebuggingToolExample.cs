0e7d72ba640e248582413d3e8cf1f4743b4261c1
Kain Shepard <kainkun100@gmail.com>
2021-02-20 18:45:59 -0800

    set up input for player and also created a tool for easy debugging and testing

---
+++ Assets/DebuggingToolExample.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebuggingToolExample : MonoBehaviour
+{
+
+    //Here is where you connect your debugging tool
+    // you can use OnZeroKey, OnOneKey to OnNineKey 
+    public void OnOneKey() //function that runs when you press the one key
+    {
+        YourFunctionYouWantToTest();
+    }
+    public void OnTwoKey() //function that runs when you press the two key
+    {
+        print(AnotherFunctionToTest()); //if you want to see what your function outputs, just put it in a print()
+    }
+
+
+    //Below this is your normal work that you want to test
+
+    public void YourFunctionYouWantToTest()
+    {
+        print("first function");
+    }
+
+    private bool AnotherFunctionToTest()
+    {
+        print("second function");
+        return true;
+    }
+}
---
+++ Assets/Scripts/DebuggingInputTool.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DebuggingInputTool : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.LogWarning("DebuggingInputTool in use, delete before build");
+    }
+}
