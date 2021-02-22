2b7f6dbc3ff9b00ea1353d064f03b52430496144
Josh <jsantos8153@sdsu.edu>
2021-02-21 21:44:17 -0800

    made camera shaking a little better. changed some starting variables. comments about functions  conflicting with future scripts.

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -21,13 +21,12 @@ public class CameraShake : MonoBehaviour{
     void Start(){
         instance = this;
         maxOffset = 0.1f;
-        frequency = 20.0f;
-        rotationMultiplier = 10.0f;
+        frequency = 8.0f;
+        rotationMultiplier = 7.0f;
     }
     private void LateUpdate(){
         if (shakeTimeRemaining > 0){
             shakeTimeRemaining -= Time.deltaTime;
-
 
             Vector3 offset = new Vector3(GetPerlinNoise(1) * maxOffset, GetPerlinNoise(2) * maxOffset, 0f);
 
@@ -38,6 +37,7 @@ public class CameraShake : MonoBehaviour{
             transform.localRotation = Quaternion.Euler(0f, 0f, GetPerlinNoise(3) * 2 * rotationMultiplier);
 
             // Resets the cameras position and rotation to assumed values before the shake.
+            // When shakeTimeRemaining has reached 0, this will run once.
             if (shakeTimeRemaining <= 0){
                 transform.position = AssumedCamPos();
                 transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
@@ -49,11 +49,13 @@ public class CameraShake : MonoBehaviour{
         return (Mathf.PerlinNoise(Time.time * frequency, seed) * 2 - 1);
     }
 
-    // GetPlayerPos() - Returns the Cameras assumed location, relative to the players position.  
+    // GetPlayerPos() - Returns the Cameras assumed location, relative to the players position.
+    // if there is a script that follows the player, this function might not be needed.
     private Vector3 AssumedCamPos(){
         return new Vector3(player.position.x, player.position.y, transform.position.z);
     }
 
+    // Other varibles like frequency could be add to this StartShake function later. 
     [Button]
     public void StartShake(float time){
         shakeTimeRemaining = time;
