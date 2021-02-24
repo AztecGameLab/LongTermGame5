f85be9642afe5fe2c8305ce10f1e8957f66f9200
Yousif <yousifjabbo@gmail.com>
2021-02-23 22:10:13 -0800

    Initial commit of flying enemy
    Added flying enemy main components.

---
+++ Assets/Scripts/Enemies/FlyingEnemyMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemyMovement : MonoBehaviour
+{
+    PlatformerController player;
+    [SerializeField] float speed;
+    [SerializeField] float playerRange;
+
+    Vector3 amountToMove;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlatformerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position,player.transform.position, speed*Time.deltaTime);
+    
+    }
+}
