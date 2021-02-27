882281d91f99c199a7e8c4bd25618eb06721bcd3
EdenArts <ojedaeden08@gmail.com>
2021-02-27 13:28:07 -0800

    Grapple hooks onto the detected collision

---
+++ Assets/Scripts/JointGrapple.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointGrapple : MonoBehaviour
+{
+    
+    public Vector2 grappleFire;
+    public SpringJoint2D spring;
+    public LayerMask whatIsGrapplable;
+
+
+    private void Update()
+    {
+        //spring.attachedRigidbody.position.x -= 1;
+      //  spring.attachedRigidbody.position.y -= 1;
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartGrapple();
+        }
+        else if (Input.GetMouseButtonUp(0))
+            StopGrapple();
+        }
+
+    private void StartGrapple()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Input.mousePosition, whatIsGrapplable);
+        
+        if (hit.collider != null)
+        {
+            Debug.Log("ray casted");
+            Debug.DrawRay(transform.position, Input.mousePosition, Color.green);
+            if (hit.rigidbody != null)
+            {
+                Debug.Log("rigid body hit");
+                Vector3 coll = hit.collider.ClosestPoint(hit.point);
+                spring.connectedAnchor = coll;
+            }
+        }
+
+
+    }
+
+    private void StopGrapple()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/SpringGrapple.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringGrapple : MonoBehaviour
+{
+    private SpringJoint2D player;
+    public void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            player = GameObject.FindGameObjectWithTag("Player").GetComponent<SpringJoint2D>();
+            player.connectedBody = GetComponent<Rigidbody2D>();
+        }
+    }
+}
