d883e749f702318341f40698b7d65905da06ef62
Kain Shepard <kainkun100@gmail.com>
2021-02-28 12:14:32 -0800

    save point

---
+++ Assets/Scripts/SaveSystem/SavePoint.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace SaveSystem
+{
+    public class SavePoint : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
