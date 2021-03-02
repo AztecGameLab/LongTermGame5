9fb72245cc06ca8f434a4b120cc3985e62f9b74d
Sam Schneider <samuel.m.schneider@gmail.com>
2021-03-02 10:39:03 -0800

    added a Entity subclass to store position and amount of times it has been spawned

--- Assets/Scripts/EntitySpawner/EntitySpawner.cs
+++ Assets/Scripts/EntitySpawner/EntitySpawner.cs
@@ -5,28 +5,38 @@ using UnityEngine;
 
 public class EntitySpawner : MonoBehaviour
 {
-    [SerializeField]List<GameObject> Entities;      //Entities that are in the Spawn Group
+    [SerializeField] List<GameObject> Entities;     //Entities that are in the Spawn Group
+    [SerializeField] List<Transform> Positions;     //Locations to spawn each Entity
     [SerializeField] Boolean SpawnOnStart;          //Spawn Entities when level loads
     [SerializeField] Boolean SpawnOnTrigger;        //Spawn Entities on trigger
-    [SerializeField] Collider2D SpawnCollide;       //Collider used as trigger to spawn Entities
+    [SerializeField] Collider2D SpawnCollider;      //Collider used as trigger to spawn Entities
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
-       
+          
     }
 
     //Spawns an entity
-    void Spawn(GameObject Entity)
+    void Spawn(GameObject Entity, Transform pos)
     {
         //spawns given entity at given transform(position)
-        Instantiate(Entity, Entity.transform);
+        Instantiate(Entity, pos);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnValidate()
+    {
+        foreach(GameObject Entity in Entities)
+        {
+            Spawn(Entity, Entity.transform);
+        }
     }
 }
