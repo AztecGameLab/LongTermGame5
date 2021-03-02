18d4a9b7511863a2085177338a92544eae867aaf
Sam Schneider <samuel.m.schneider@gmail.com>
2021-03-01 19:22:30 -0800

    created GameObject and script EntitySpawner; add list variable to store entities

---
+++ Assets/Scripts/EntitySpawner/EntitySpawner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntitySpawner : MonoBehaviour
+{
+    //Entities that will spawn
+    [SerializeField]List<GameObject> Entities;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
