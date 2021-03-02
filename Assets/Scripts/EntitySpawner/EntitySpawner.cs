f5bbf4eb45aa7083d3961583bd2c08b52507f732
Sam Schneider <samuel.m.schneider@gmail.com>
2021-03-01 19:34:11 -0800

    Created SubClass Entities to store the GameObject along with position

--- Assets/Scripts/EntitySpawner/EntitySpawner.cs
+++ Assets/Scripts/EntitySpawner/EntitySpawner.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 
 public class EntitySpawner : MonoBehaviour
 {
-    //Entities that will spawn
-    [SerializeField]List<GameObject> Entities;
-
+    public class Entity
+    {
+        public GameObject Object;
+        public Transform pos;
+    }
+  
+    [SerializeField]List<Entity> Entities;      //Entities that are in the Spawn Group
+    [SerializeField] Boolean SpawnOnStart;          //Spawn Entities when level loads
+    [SerializeField] Boolean SpawnOnTrigger;        //Spawn Entities on trigger
+    [SerializeField] Collider2D SpawnCollide;       //Collider used as trigger to spawn Entities
 
 
     // Start is called before the first frame update
