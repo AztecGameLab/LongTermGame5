4c6ec8a27a8feb7c8f9ea57fe436577a69179b4b
Brandon Altamirano <manabran501@hotmail.com>
2021-03-03 12:57:40 -0800

    Update ProjectileWeapon.cs
    Deleted createAssetMenu from projectileWeapon, back to original script

--- Assets/Scripts/BaseCode/ProjectileWeapon.cs
+++ Assets/Scripts/BaseCode/ProjectileWeapon.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Data", menuName = "FireBallStats", order = 1)]
+
 public class ProjectileWeapon : ScriptableObject
 {
     
