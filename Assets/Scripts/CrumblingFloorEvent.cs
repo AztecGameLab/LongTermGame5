dbb1ee4e320072ac0759ca1f7572e74a827875d4
Cole Kincaid <ckincaid6921@sdsu.edu>
2021-03-03 15:48:18 -0800

    added crumbling floor prefab
    CrumblingFloorEvent script is attached to child gameobject of the crumbling floor, when collision is triggered by a player tagged object there is a delay, which can be changed in the inspector, then the parent crumblingfloor gameobject is destroyed.

---
+++ Assets/Scripts/CrumblingFloorEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrumblingFloorEvent : MonoBehaviour
+{
+    public float crumbleDelay;
+
+    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("there was a collision!");
+        if (collision.CompareTag("Player"))
+        {
+
+            Debug.Log("destroying object after one second");
+            Destroy(gameObject.transform.parent.gameObject, crumbleDelay);
+        }
+    }
+}
