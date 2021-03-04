c631ec3ede66dba7a9ad8988bfed36194d72f0db
Cole Kincaid <ckincaid6921@sdsu.edu>
2021-03-03 18:13:54 -0800

    Added Sound Effect upon collision with platform
    -setting the delay of the crumble to 1.35 syncs well with the audio clip provided

--- Assets/Scripts/CrumblingFloorEvent.cs
+++ Assets/Scripts/CrumblingFloorEvent.cs
@@ -6,14 +6,14 @@ public class CrumblingFloorEvent : MonoBehaviour
 {
     public float crumbleDelay;
 
-    
+    public AudioClip crumbling;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("there was a collision!");
         if (collision.CompareTag("Player"))
         {
-
+            AudioSource.PlayClipAtPoint(crumbling,gameObject.transform.position);
             Debug.Log("destroying object after one second");
             Destroy(gameObject.transform.parent.gameObject, crumbleDelay);
         }
