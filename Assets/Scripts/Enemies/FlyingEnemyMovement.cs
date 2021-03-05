f97f71400251ae22ad640ad9c12771aeb0156d6a
Yousif <yousifjabbo@gmail.com>
2021-03-04 17:40:25 -0800

    Improved Coroutines
    I no longer use the update method to change the behavior of the Enemy.

--- Assets/Scripts/Enemies/FlyingEnemyMovement.cs
+++ Assets/Scripts/Enemies/FlyingEnemyMovement.cs
@@ -6,19 +6,17 @@ using UnityEngine;
 public class FlyingEnemyMovement : MonoBehaviour
 {
     PlatformerController player;
-    [SerializeField] float speed;
-    [SerializeField] float amountToMove;
-    [SerializeField] float FireRate = 1f;
-    [SerializeField] Transform ProjectileSpawnPoint;
-    [SerializeField] GameObject Projectile;
-
-
+    [SerializeField] private float speed;
+    [SerializeField] private float amountToMove;
+    [SerializeField] private float FireRate = 1f;
+    [SerializeField] private Transform ProjectileSpawnPoint;
+    [SerializeField] private GameObject Projectile;
 
     bool IsAttacking = false;
-    bool CanFire = true;
     bool CanMove = true;
     float rotate = 180;
     Rigidbody2D rb;
+    
     
 
     // Start is called before the first frame update
@@ -26,33 +24,18 @@ public class FlyingEnemyMovement : MonoBehaviour
     {
         player = FindObjectOfType<PlatformerController>();
         rb = GetComponent<Rigidbody2D>();
+        PeacefulPattern();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if (IsAttacking)
-        {
-            //1-stop, 2-look at player, 3-start shooting Projectile
-            rb.velocity = new Vector2(0, 0);
-            //look at player
-            Vector3 rotation = Quaternion.LookRotation(player.transform.position).eulerAngles;
-            rotation.y = 0;
-            rotation.x = 0;
-            transform.rotation = Quaternion.Euler(rotation);
 
-            AttackPattern();
-        }
-        else
-        {
-            PeacefulPattern();
-        }
-    
     }
 
     private void PeacefulPattern()
     {
+
         if (CanMove)
         {
             StartCoroutine(RLMovment());
@@ -62,36 +45,52 @@ public class FlyingEnemyMovement : MonoBehaviour
 
     private void AttackPattern()
     {
-        if (CanFire)
-        {
-            StartCoroutine(Shoot());
-        }
+        //1-stop, 2-look at player, 3-start shooting Projectile
+        rb.velocity = new Vector2(0, 0);
+        LookatPlayer();
+        StartCoroutine(Shoot());
 
     }
+
+    private void LookatPlayer()
+    {
+        Vector3 rotation = Quaternion.LookRotation(player.transform.position).eulerAngles;
+        rotation.y = 0;
+        rotation.x = 0;
+        transform.rotation = Quaternion.Euler(rotation);
+    }
+
     IEnumerator RLMovment()
     {
-        CanMove = false;
-        //flip right to left by adding nagative velocity
-        
-        rotate += 180;
-        speed *= -1f;
-        transform.localRotation = Quaternion.Euler(0, rotate, 0);
-        rb.velocity = new Vector2(speed, 0);
-        yield return new WaitForSeconds(amountToMove);
-        CanMove = true;
+        while (!IsAttacking)
+        {
+            CanMove = false;
+            //flip right to left by adding nagative velocity
+
+            rotate += 180;
+            speed *= -1f;
+            transform.localRotation = Quaternion.Euler(0, rotate, 0);
+            rb.velocity = new Vector2(speed, 0);
+            yield return new WaitForSeconds(amountToMove);
+            CanMove = true;
+        }
     }
     IEnumerator Shoot()
     {
-        CanFire = false;
-        Instantiate(Projectile, ProjectileSpawnPoint.position, ProjectileSpawnPoint.rotation);
-        yield return new WaitForSeconds(FireRate);
-        CanFire = true;
+        while (IsAttacking)
+        {
+            LookatPlayer();
+            Instantiate(Projectile, ProjectileSpawnPoint.position, ProjectileSpawnPoint.rotation);
+            yield return new WaitForSeconds(FireRate);
+
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.GetComponent<PlatformerController>())
         {
             IsAttacking = true;
+            AttackPattern();
         }
 
     }
@@ -100,6 +99,7 @@ public class FlyingEnemyMovement : MonoBehaviour
         if (collision.GetComponent<PlatformerController>())
         {
             IsAttacking = false;
+            PeacefulPattern();
         }
     }
 
