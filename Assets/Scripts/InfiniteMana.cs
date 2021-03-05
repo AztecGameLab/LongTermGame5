9bae59e3abac90672fbdc77b2d78a66c3ce31a19
Joseph Tomada <65485399+j-tomada@users.noreply.github.com>
2021-03-05 14:05:22 -0800

    Overrided TakeDamage
    Overrided TakeDamage method from unity
    
    Whenever object takes damage, it should reset its hp back to full, therefore, not taking damage

---
+++ Assets/Scripts/InfiniteMana.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiniteMana : Entity
+{
+    private float maxHealth;
+    private static System.Random rand = new System.Random();
+    public override void TakeDamage(float baseDamage)
+    {
+        base.TakeDamage(baseDamage);
+        for (int i = 0; i < rand.Next(1, 4); ++i) //Generate 1 - 3 mana orbs
+        {
+            //TODO: Instantiate mana prefabs
+        }
+        base.health = maxHealth; //Health resets back to full 
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = base.health;
+    }
+
+}
