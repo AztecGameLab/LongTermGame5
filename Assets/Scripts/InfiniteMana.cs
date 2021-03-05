1fab3b7c4107df7ba6ba92e67bc5712f8512d886
Joseph Tomada <65485399+j-tomada@users.noreply.github.com>
2021-03-05 14:57:51 -0800

    TakeDamage now instaciates ManaOrb prefab
    Created a base for the ManaOrb Prefab
    
    TakeDamage Method will now instanciate 1 - 3 mana orbs on the scene if it takes damage

--- Assets/Scripts/InfiniteMana.cs
+++ Assets/Scripts/InfiniteMana.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 
 public class InfiniteMana : Entity
 {
+    public GameObject manaOrb;
     private float maxHealth;
+    private float spawnerCordsX;
+    private float spawnerCordsY;
     private static System.Random rand = new System.Random();
+
     public override void TakeDamage(float baseDamage)
     {
         base.TakeDamage(baseDamage);
+
+        Vector2 orbPosition;
         for (int i = 0; i < rand.Next(1, 4); ++i) //Generate 1 - 3 mana orbs
         {
-            //TODO: Instantiate mana prefabs
+            orbPosition = new Vector2(spawnerCordsX + i + 3, spawnerCordsY); //Makes it so that orbs are separated from each other; Testing purposes
+            Instantiate(manaOrb, orbPosition, Quaternion.identity);
         }
         base.health = maxHealth; //Health resets back to full 
     }
@@ -20,7 +27,17 @@ public class InfiniteMana : Entity
     // Start is called before the first frame update
     void Start()
     {
+        spawnerCordsX = gameObject.GetComponent<Transform>().position.x;
+        spawnerCordsY = gameObject.GetComponent<Transform>().position.y;
         maxHealth = base.health;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            TakeDamage(100);
+        }
     }
 
 }
