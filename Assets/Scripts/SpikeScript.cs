9ced13df7f70e9e4b48b0629bdee6528e9f56f5e
Joshua Santos <jsantos8153@sdsu.edu>
2021-03-06 13:45:05 -0800

    on collision, object now takes damage, x-axis still needs to be manipulated

--- Assets/Scripts/SpikeScript.cs
+++ Assets/Scripts/SpikeScript.cs
@@ -8,6 +8,10 @@ public class SpikeScript : MonoBehaviour
     [SerializeField] private float timer;
     public PolygonCollider2D poly;
 
+
+    /***********************************************************************************************
+     * SpikeScript 
+     **********************************************************************************************/
     private void Awake() {
         poly = this.GetComponent<PolygonCollider2D>();
         poly = this.gameObject.AddComponent<PolygonCollider2D>();
@@ -15,26 +19,31 @@ public class SpikeScript : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
         poke = 5f;
         timer = 0.5f;
-        Debug.Log("ouch a spike...");   // Player takes damage.
+
+        Entity entity = other.GetComponent<Entity>();
+        if (entity != null) {
+            entity.TakeDamage(1f); // Player takes damage.
+            Debug.Log(entity.health);
+        }
         Rigidbody2D person = other.GetComponent<Rigidbody2D>();
-       
-        if(person != null) {
+        if (person != null) {
             Vector2 direction = person.transform.position - transform.position;
+            //Vector2 direction = other.GetContacts() - GetContacts;
             direction = direction.normalized * poke;
             person.AddForce(direction, ForceMode2D.Impulse);
             if(direction.x < 0) {
-                Debug.Log("Move transform Left");
-                while (timer > 0) {
-                    person.AddForce(new Vector3(-20f, 1f, 0f));
-                    timer -= Time.deltaTime;
-                }
+                //Debug.Log("Move transform Left");
+                //while (timer > 0) {
+                //    person.AddForce(new Vector3(-20f, 1f, 0f));
+                //    timer -= Time.deltaTime;
+                //}
                 // move on x-axis to the left
             } else {
-                Debug.Log("Move transform right");
-                while (timer > 0){
-                    person.AddForce(new Vector3(20f,1f,0f));
-                    timer -= Time.deltaTime;
-                }
+                //Debug.Log("Move transform right");
+                //while (timer > 0){
+                //    person.AddForce(new Vector3(20f,1f,0f));
+                //    timer -= Time.deltaTime;
+                //}
                 // move on x-axis to the right
             }
         }
