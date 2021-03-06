09f44eebdb29c9a529dd5b4f506155bb1979712d
Joshua Santos <jsantos8153@sdsu.edu>
2021-03-06 01:21:14 -0800

    spikes work for Y-axis knock back.

--- Assets/Scripts/SpikeScript.cs
+++ Assets/Scripts/SpikeScript.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class SpikeScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float poke;
+    public PolygonCollider2D poly;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Awake() {
+        poly = this.GetComponent<PolygonCollider2D>();
+        poly = this.gameObject.AddComponent<PolygonCollider2D>();
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        poke = 5f;
+        Debug.Log("ouch a spike...");   // Player takes damage.
+        Rigidbody2D person = other.GetComponent<Rigidbody2D>();
+        if(person != null) {
+            Vector2 direction = person.transform.position - transform.position;
+            direction = direction.normalized * poke;
+            person.AddForce(direction, ForceMode2D.Impulse);
+        }
     }
 }
