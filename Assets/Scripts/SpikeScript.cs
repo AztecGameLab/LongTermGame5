ca459f797b9594ddb3187f1c240289f354fca675
Joshua Santos <jsantos8153@sdsu.edu>
2021-03-07 02:42:22 -0800

    testing some variables, Trying to make code readable.

--- Assets/Scripts/SpikeScript.cs
+++ Assets/Scripts/SpikeScript.cs
@@ -4,35 +4,29 @@ using UnityEngine;
 
 public class SpikeScript : MonoBehaviour
 {
-    [SerializeField] private Vector2 poke;
+    [SerializeField] private float x;
+    [SerializeField] private float y;
     [SerializeField] private float spikeDamage;
-    [SerializeField] private float timer;
-
-    private PolygonCollider2D poly;
 
     void Awake() {
-        poly = GetComponent<PolygonCollider2D>();
-        poly = gameObject.AddComponent<PolygonCollider2D>();
-        poke = new Vector2(3f, 3f);
+        this.gameObject.AddComponent<PolygonCollider2D>();
+        x = 4f;
+        y = 3f;
         spikeDamage = 1f;
-        timer = 0.1f;
     }
     void OnCollisionEnter2D(Collision2D other) {   
         if (other.gameObject.CompareTag("Player")) {
 
-            Rigidbody2D person = other.rigidbody;
-            ContactPoint2D contacted = other.GetContact(0);
             Vector2 playerPos = other.transform.position;
+            ContactPoint2D contacted = other.GetContact(0);
             Vector2 direction =  playerPos - contacted.point;
-            direction = direction.normalized * poke;
-            Debug.Log(direction);
-            person.AddForce(direction, ForceMode2D.Impulse);
+            direction = direction.normalized * new Vector2(x, y);
 
-            Entity entity = person.GetComponentInParent<Entity>();
-            Debug.Log(entity);
+            Rigidbody2D player = other.rigidbody;
+            player.AddForce(direction, ForceMode2D.Impulse);
+
+            Entity entity = player.GetComponentInParent<Entity>();
             entity.TakeDamage(spikeDamage);
-
-
         }
     }
 }
