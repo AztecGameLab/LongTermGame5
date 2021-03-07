09f099f86ec5d1adae32c2e42d3c89bacff8239e
Joshua Santos <jsantos8153@sdsu.edu>
2021-03-06 18:06:27 -0800

    Started experimenting with gravity. seem to be making progress.

--- Assets/Scripts/SpikeScript.cs
+++ Assets/Scripts/SpikeScript.cs
@@ -17,57 +17,37 @@ public class SpikeScript : MonoBehaviour
         poly.isTrigger = true;
     }
     private void Start() {
-        poke = 1f;
+        poke = 5f;
         damage = 1f;
+        timer = 0.3f;
     }
     private void OnTriggerEnter2D(Collider2D other) {
-        timer = 1f;
 
         Rigidbody2D person = other.GetComponent<Rigidbody2D>();
         Entity entity = other.GetComponent<Entity>();
-        poly.isTrigger = false;
+
         if (entity != null) {
             entity.TakeDamage(damage); // Player takes damage.
             Debug.Log(entity.health);
         }
 
         if (person != null) {
+            person.gravityScale = 0;
             Vector2 direction = person.transform.position - transform.position;
             //int coor = other.GetContacts;
             direction = direction.normalized * poke;
             Debug.Log(direction);
-            Vector2 target = (Vector2)person.transform.position + direction;
-            Debug.Log("Player pos"+ person.transform.position);
-            Debug.Log("Target pos"+ target);
-            //person.AddForce(direction, ForceMode2D.Impulse);
-            //person.velocity = new Vector2(-50f, person.velocity.y);
-            StartCoroutine(Knocked(person, other, target));
-            if (direction.x < 0) {
-                //Debug.Log("Move transform Left");
-                //while (timer > 0) {
-                //   person.velocity = new Vector2(-50f,person.velocity.y);
-                //   timer -= Time.deltaTime;
-                //}
-                // move on x-axis to the left
-                
-            } else {
-                //Debug.Log("Move transform right");
-                //while (timer > 0){
-                //    person.velocity = new Vector2(50f, person.velocity.y);
-                //    timer -= Time.deltaTime;
-                //}
-                // move on x-axis to the right
-            }
+            StartCoroutine(Knocked(direction, person));
+            person.AddForce(direction, ForceMode2D.Impulse);
         }
-        poly.isTrigger = true;
+        
+    }
+    private IEnumerator Knocked(Vector2 direction, Rigidbody2D person) {
+        if(person != null) {
+            yield return new WaitForSeconds(timer);
+            person.velocity = Vector2.zero;
+            person.gravityScale = 1;
+        }
     }
 
-    private IEnumerator Knocked(Rigidbody2D person, Collider2D other,Vector2 target) {
-        while(Vector2.Distance(person.transform.position, target) > 0.05f) {
-            person.transform.position = Vector2.Lerp(person.transform.position, target, Time.deltaTime/1000);
-           
-        }
-        Debug.Log("End Pos" + person.transform.position);
-        yield return null;
-    }
 }
