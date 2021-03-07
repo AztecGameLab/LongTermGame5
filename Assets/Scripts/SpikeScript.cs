9f08876331ae675444b3775a2add04f6cd33963a
Joshua Santos <jsantos8153@sdsu.edu>
2021-03-06 22:58:54 -0800

    Changed to OnCollision, Removed Platform Controller. Now x-axis seems to work.

--- Assets/Scripts/SpikeScript.cs
+++ Assets/Scripts/SpikeScript.cs
@@ -4,50 +4,30 @@ using UnityEngine;
 
 public class SpikeScript : MonoBehaviour
 {
-    [SerializeField] private float poke;
-    [SerializeField] private float timer;
+    [SerializeField] private Vector2 poke;
     [SerializeField] private float damage;
-    private float smoothing = 7f;
+    [SerializeField] private float timer;
 
     private PolygonCollider2D poly;
 
-    private void Awake() {
-        poly = this.GetComponent<PolygonCollider2D>();
-        poly = this.gameObject.AddComponent<PolygonCollider2D>();
-        poly.isTrigger = true;
-    }
-    private void Start() {
-        poke = 5f;
+    void Awake() {
+        poly = GetComponent<PolygonCollider2D>();
+        poly = gameObject.AddComponent<PolygonCollider2D>();
+        poke = new Vector2(3f, 3f);
         damage = 1f;
-        timer = 0.3f;
+        timer = 0.1f;
     }
-    private void OnTriggerEnter2D(Collider2D other) {
+    void OnCollisionEnter2D(Collision2D other) {   
+        if (other.gameObject.CompareTag("Player")) {
 
-        Rigidbody2D person = other.GetComponent<Rigidbody2D>();
-        Entity entity = other.GetComponent<Entity>();
-
-        if (entity != null) {
-            entity.TakeDamage(damage); // Player takes damage.
-            Debug.Log(entity.health);
-        }
-
-        if (person != null) {
-            person.gravityScale = 0;
-            Vector2 direction = person.transform.position - transform.position;
-            //int coor = other.GetContacts;
+            Rigidbody2D person = other.rigidbody;
+            ContactPoint2D contacted = other.GetContact(0);
+            Vector2 playerPos = other.transform.position;
+            Vector2 direction =  playerPos - contacted.point;
             direction = direction.normalized * poke;
             Debug.Log(direction);
-            StartCoroutine(Knocked(direction, person));
             person.AddForce(direction, ForceMode2D.Impulse);
-        }
-        
-    }
-    private IEnumerator Knocked(Vector2 direction, Rigidbody2D person) {
-        if(person != null) {
-            yield return new WaitForSeconds(timer);
-            person.velocity = Vector2.zero;
-            person.gravityScale = 1;
-        }
-    }
 
+        }
+    }
 }
