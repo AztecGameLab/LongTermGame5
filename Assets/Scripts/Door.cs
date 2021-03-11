d9e5ac4acdc5cd77d286c76a7cdcf0a2a5898391
Yousif <yousifjabbo@gmail.com>
2021-03-11 15:49:41 -0800

    initial commit

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : Entity
+{
+    [Header("Destructible From: ")]
+    [SerializeField] bool TheLeft = false;
+    [SerializeField] bool TheRight = false;
+    [SerializeField] bool Up = false;
+    [SerializeField] bool Down = false;
+
+
+    [EasyButtons.Button]
+    public override void TakeDamage(float baseDamage, Vector2 direction)
+    {
+
+        if ((TheLeft && direction.x < 0)|| (TheRight && direction.x > 0))
+        {
+            base.TakeDamage(baseDamage, direction);
+        }
+        if ((Down && direction.y < 0) || (Up && direction.y > 0))
+        {
+            base.TakeDamage(baseDamage, direction);
+        }
+
+       
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+
+}
