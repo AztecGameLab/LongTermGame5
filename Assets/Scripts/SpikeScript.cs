a16efe6521d614fff9547ed51af2b54148dd9e50
Joshua Santos <jsantos8153@sdsu.edu>
2021-03-12 21:39:33 -0800

    Looks good for now. made some formatting changes.

--- Assets/Scripts/SpikeScript.cs
+++ Assets/Scripts/SpikeScript.cs
@@ -7,16 +7,17 @@ using UnityEngine;
  * spike will always be pushed at a 45 degree angle. Intensity is the force multiplier. 
  * Do note, if intensity > 5, then knockback will be greater than the players actual jump height.
  * Also, this script doesnt disable the players jumping ability. So the player can jump and move
- * while being knockedback.
+ * while being knockedback from the center of the spike object.
  **************************************************************************************************/
-public class SpikeScript : MonoBehaviour
-{
+public class SpikeScript : MonoBehaviour {
+
     [SerializeField] private float intensity = 5;
     [SerializeField] private float spikeDamage = 1;     //TODO :: spikeDamage will probably be greater than 1
 
     void OnCollisionEnter2D(Collision2D other) {
 
         if (other.gameObject.GetComponent<PlatformerController>()) {
+
             Vector2 direction = (Vector2)other.transform.position - (Vector2)transform.position;
             direction = new Vector2(Mathf.Sign(direction.x), Mathf.Sign(direction.y));  // 45 degree 
     
