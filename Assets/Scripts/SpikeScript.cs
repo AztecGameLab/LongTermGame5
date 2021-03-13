4c7f421b16103ce5e5e56695c3631dcd11c5a2de
Joshua Santos <jsantos8153@sdsu.edu>
2021-03-12 21:20:50 -0800

    changed if to not rely on the "Player" tag, instead using "PlatformerController"

--- Assets/Scripts/SpikeScript.cs
+++ Assets/Scripts/SpikeScript.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /***************************************************************************************************
- * Requires that the player object to have the tag "Player". Each spike will receive a BoxCollider 
- * and is set on awake to a specific offset and size. The player OnCollision with a spike will 
- * always be pushed at a 45 degree angle. Intensity is multiplied by the signed Vector. 
+ * Requires that the player object to have the PlatformerController. The player OnCollision with a 
+ * spike will always be pushed at a 45 degree angle. Intensity is the force multiplier. 
  * Do note, if intensity > 5, then knockback will be greater than the players actual jump height.
  * Also, this script doesnt disable the players jumping ability. So the player can jump and move
  * while being knockedback.
@@ -17,13 +16,12 @@ public class SpikeScript : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other) {
 
-        if (other.gameObject.CompareTag("Player")) {
+        if (other.gameObject.GetComponent<PlatformerController>()) {
             Vector2 playerPos = other.transform.position;
             ContactPoint2D contacted = other.GetContact(0);  
             Vector2 direction =  playerPos - contacted.point;
             direction = new Vector2(Mathf.Sign(direction.x), Mathf.Sign(direction.y));  // 45 degree 
     
-            //Debug.DrawLine(playerPos,playerPos+direction);
             var player = PlatformerController.instance;
             player.TakeDamage(spikeDamage);
             player.KnockBack(direction, intensity);
