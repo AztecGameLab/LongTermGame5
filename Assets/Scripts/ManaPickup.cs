5ed945a5c6ccfc703a1808d56be7b7fd70419199
Joseph Tomada <65485399+j-tomada@users.noreply.github.com>
2021-03-17 21:49:41 -0700

    Added ManaPickup.cs
    Will be used on the mana orb prefab

---
+++ Assets/Scripts/ManaPickup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManaPickup : ManaSystem
+{
+    public float mana; //How much mana each orb will have
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
