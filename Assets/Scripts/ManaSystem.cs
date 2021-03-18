b91d6479c98ad0aa3b5c6be6467a7b315b3d121b
Kain Shepard <kainkun100@gmail.com>
2021-03-17 18:47:56 -0700

    ManaSystem wrapper with singleton for FillBarSystem

---
+++ Assets/Scripts/ManaSystem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManaSystem : FillSystem
+{
+    public static ManaSystem instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+}
