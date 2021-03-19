d2ac9dfcaae7afd02630fde166bcdab2b81c3900
EdenArts <ojedaeden08@gmail.com>
2021-03-19 10:31:03 -0700

    added scripts for coroutines

---
+++ Assets/Scripts/GrappDriver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrappDriver : MonoBehaviour
+{
+    public GrappScript Grapp;
+    Vector2 direction;
+ 
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        Grapp.Charge(direction);
+    }
+}
---
+++ Assets/Scripts/GrappScript.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrappScript : ProjectileWeapon
+{
+    SpringJoint2D joint;
+    Vector2 grapplePoint;
+    public LineRenderer lr;
+    public float grappleRange = 100, grappleTightness = 2, dampingRatio = 1, grappleSpeed = 0;
+
+    override
+    public void Charge(Vector2 direction)
+    {
+        Debug.Log("entered Charge");
+        PlatformerController.instance.StartCoroutine(StartGrapple(direction));
+    }
+
+    public IEnumerator StartGrapple(Vector2 direction)
+    {
+        Vector2 origin = GameObject.FindGameObjectWithTag("Player").transform.position;
+
+        RaycastHit2D hit = Physics2D.Raycast(origin, direction);
+        //Do something
+        while (Input.GetMouseButtonDown(0))
+        {
+            if (hit.collider != null)
+            {
+                //Debug.Log("collider hit");
+
+                joint = GameObject.FindGameObjectWithTag("Player").gameObject.AddComponent<SpringJoint2D>();
+
+                grapplePoint = hit.collider.ClosestPoint(hit.point);
+
+                joint.enableCollision = true;
+                joint.autoConfigureConnectedAnchor = false;                         //grapple settings
+                joint.connectedAnchor = grapplePoint;
+
+                // float distanceFromPoint = Vector2.Distance(player.position, grapplePoint);
+
+                joint.frequency = grappleSpeed;
+                joint.distance = grappleTightness;
+                joint.dampingRatio = dampingRatio;
+
+                lr.positionCount = 2;
+                //Do something repetitively 
+                yield return null; //Wait for next update loop
+            }//I can still put code here
+        }
+    }
+}
