212d1b2e48906fe5b7baa822925a9650711479d5
Cole Kincaid <ckincaid6921@sdsu.edu>
2021-03-18 17:40:15 -0700

    Updated Crumbling Floor Script
    Now the platform triggers the warning sound when jumped on then begins the crumbling (destruction of the platform) when the player has been on the platform for 1 second.
    
    Currently trying to fix the audio for when the floor begins to fall apart because at the moment it spamming the crumblingfloor audio clip while touching the platform during the crumbling phase.

--- Assets/Scripts/CrumblingFloorEvent.cs
+++ Assets/Scripts/CrumblingFloorEvent.cs
@@ -8,14 +8,54 @@ public class CrumblingFloorEvent : MonoBehaviour
 
     public AudioClip crumbling;
 
+    public AudioClip crumblingWarning;
+
+    public float timerCountDown = 1;
+
+    bool isColliding = false;
+
+    private void Update()
+    {
+        if (isColliding == true)
+        {
+            timerCountDown -= Time.deltaTime;
+            if (timerCountDown < 0)
+            {
+                timerCountDown = 0;
+            }
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("there was a collision!");
-        if (collision.CompareTag("Player"))
+        if (collision.gameObject.tag == "Player")
         {
-            AudioSource.PlayClipAtPoint(crumbling, gameObject.transform.position);
-            Debug.Log("destroying object after one second");
-            Destroy(gameObject.transform.parent.gameObject, crumbleDelay);
+            Debug.Log("player has come in contact with the platform");
+            AudioSource.PlayClipAtPoint(crumblingWarning, gameObject.transform.position);
+            isColliding = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            if(timerCountDown <= 0)
+            {
+                AudioSource.PlayClipAtPoint(crumbling, gameObject.transform.position);
+                Debug.Log("destroying object after one second");
+                Destroy(gameObject.transform.parent.gameObject, crumbleDelay);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Player has left platform before colllapse");
+            isColliding = false;
+            timerCountDown = 1;
         }
     }
 }
