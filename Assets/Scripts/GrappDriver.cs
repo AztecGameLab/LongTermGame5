fd6d4d5d063e7f04e8dfb55c33ade2c847ea0676
EdenArts <ojedaeden08@gmail.com>
2021-03-19 12:21:46 -0700

    made DrawRope a coroutine and added final touches to the new GrappleScript

--- Assets/Scripts/GrappDriver.cs
+++ Assets/Scripts/GrappDriver.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class GrappDriver : MonoBehaviour
 {
-    public GrappScript Grapp;
+    [SerializeField]  GrappScript Grapp;
     Vector2 direction;
- 
+    public Camera cam;
 
-    
+
+
+
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePos - (Vector2)transform.position;
 
-        Grapp.Charge(direction);
+        if (Input.GetMouseButtonDown(0))
+            Grapp.Fire(direction);
+
+        else if (Input.GetMouseButtonUp(0))
+            Grapp.EndGrapple();
     }
 }
+
--- Assets/Scripts/GrappScript.cs
+++ Assets/Scripts/GrappScript.cs
@@ -3,50 +3,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "GrappScript", menuName = "LTG5/Weapons/GrappleHook", order = 0)]
 public class GrappScript : ProjectileWeapon
 {
     SpringJoint2D joint;
     Vector2 grapplePoint;
     public LineRenderer lr;
-    public float grappleRange = 100, grappleTightness = 2, dampingRatio = 1, grappleSpeed = 0;
-
+    
     override
-    public void Charge(Vector2 direction)
+    public void Fire(Vector2 direction)
     {
-        Debug.Log("entered Charge");
-        PlatformerController.instance.StartCoroutine(StartGrapple(direction));
+        lr = GameObject.FindGameObjectWithTag("Player").GetComponent<LineRenderer>();
+
+        PlatformerController.instance.StartCoroutine(DrawRope());       
+        StartGrapple(direction);
+        
     }
 
-    public IEnumerator StartGrapple(Vector2 direction)
+    public void EndGrapple()            
     {
-        Vector2 origin = GameObject.FindGameObjectWithTag("Player").transform.position;
+        lr.positionCount = 0;
+        Destroy(joint);
+    }
 
-        RaycastHit2D hit = Physics2D.Raycast(origin, direction);
-        //Do something
-        while (Input.GetMouseButtonDown(0))
+    public void StartGrapple(Vector2 direction)
+    {
+        Vector2 playerPos = PlatformerController.instance.transform.position;
+        //Vector2 playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;      for testing while i didnt have PlatformerController on
+        RaycastHit2D hit = Physics2D.Raycast(playerPos, direction, 100);                           
+
+        Debug.DrawRay(playerPos, direction, Color.red, 10);
+        if (hit.collider != null)
         {
-            if (hit.collider != null)
-            {
-                //Debug.Log("collider hit");
+  
 
-                joint = GameObject.FindGameObjectWithTag("Player").gameObject.AddComponent<SpringJoint2D>();
+            joint = PlatformerController.instance.gameObject.AddComponent<SpringJoint2D>();
+            Debug.Log("hit something");
+            //joint = GameObject.FindGameObjectWithTag("Player").AddComponent<SpringJoint2D>();    for testing while i didnt have PlatformerController on
 
-                grapplePoint = hit.collider.ClosestPoint(hit.point);
+            grapplePoint = hit.collider.ClosestPoint(hit.point);
 
-                joint.enableCollision = true;
-                joint.autoConfigureConnectedAnchor = false;                         //grapple settings
-                joint.connectedAnchor = grapplePoint;
+            joint.enableCollision = true;
+            joint.autoConfigureConnectedAnchor = false;                         //grapple settings
+            joint.connectedAnchor = grapplePoint;
+            joint.frequency = 1;
+            joint.distance = 1;
+            joint.dampingRatio = 1;
 
-                // float distanceFromPoint = Vector2.Distance(player.position, grapplePoint);
+            lr.positionCount = 2;
+        }
 
-                joint.frequency = grappleSpeed;
-                joint.distance = grappleTightness;
-                joint.dampingRatio = dampingRatio;
+    }
 
-                lr.positionCount = 2;
-                //Do something repetitively 
-                yield return null; //Wait for next update loop
-            }//I can still put code here
+    IEnumerator DrawRope()
+    {
+        while (joint)
+        {
+            lr.SetPosition(0, GameObject.FindGameObjectWithTag("Player").transform.position);
+            lr.SetPosition(1, grapplePoint);
+            yield return null;
         }
     }
+
 }
+
+    
+
