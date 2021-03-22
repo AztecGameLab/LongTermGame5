13ff9386633d76e94063b4d6f9eede03796fd7f8
MattWant <71905800+MattWant@users.noreply.github.com>
2021-03-22 11:10:37 -0700

    Localized Wind
    Made a box collider that is a trigger which recognizes rigid bodies that enter it and applies a force at a desired speed and direction.

---
+++ Assets/Scripts/LocalizedWind.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalizedWind : MonoBehaviour
+{
+    List<Rigidbody2D> RigidbodiesinWindZoneList = new List<Rigidbody2D>();
+    public Vector3 windDirection = Vector3.right;
+    public float windStrength = 5;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("Trigger");
+        Rigidbody2D objectRigid = col.gameObject.GetComponent<Rigidbody2D>();
+        if (objectRigid != null)
+            RigidbodiesinWindZoneList.Add(objectRigid);
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        Rigidbody2D objectRigid = col.gameObject.GetComponent<Rigidbody2D>();
+        if (objectRigid != null)
+            RigidbodiesinWindZoneList.Remove(objectRigid);
+    }
+
+    private void FixedUpdate()
+    {
+        if (RigidbodiesinWindZoneList.Count > 0)
+        {
+            foreach (Rigidbody2D rigid in RigidbodiesinWindZoneList)
+            {
+                rigid.AddForce(windDirection * windStrength);
+            }
+        }
+    }
+}
