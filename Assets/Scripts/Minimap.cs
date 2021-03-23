0bb2f154878fbcbb0438f007958f1926c919dabc
James Maron <71116099+chmilk2@users.noreply.github.com>
2021-03-22 23:21:14 -0700

    Added Room list that draws on screen

--- Assets/Scripts/Minimap.cs
+++ Assets/Scripts/Minimap.cs
@@ -1,25 +1,45 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Minimap : MonoBehaviour
 {
     public CanvasGroup canvasGroup;
+    private Vector2 center = new Vector2(0,0);
 
+    private static List<Room> rooms;
     private static Texture2D backgroundTexture;
     private static GUIStyle textureStyle;
 
     // Start is called before the first frame update
     void Start()
     {
+        rooms = new List<Room>();
+        getRooms();
         backgroundTexture = Texture2D.whiteTexture;
         textureStyle = new GUIStyle { normal = new GUIStyleState { background = backgroundTexture } };
     }
 
+    private void getRooms()
+    {
+        rooms.Add(new Room(new Rect(100, 100, 50, 50), -1));
+        rooms.Add(new Room(new Rect(150, 150, 50, 50), 0));
+        rooms.Add(new Room(new Rect(200, 200, 50, 50), 1));
+        //TODO: Get rooms from bounding boxes
+    }
+
     private void OnGUI()
     {
-        Rect rect = new Rect(100,100,100,100);
-        DrawRect(rect, Color.white);
+        foreach (Room r in rooms)
+        {
+            if (r.getDiscovered() >= 0)
+            {
+                DrawRect(r.getRect(), (r.getDiscovered() == 1 ? Color.white : Color.yellow));
+            }
+            
+        }
+        
     }
 
     public static void DrawRect(Rect position, Color color, GUIContent content = null)
@@ -34,5 +54,26 @@ public class Minimap : MonoBehaviour
     void Update()
     {
         
+    }
+}
+
+class Room
+{
+    Rect roomRect;
+    int discovered = -1;
+
+    public Room(Rect rect, int discovered = -1)
+    {
+        this.roomRect = rect;
+        this.discovered = discovered;
+    }
+    public Rect getRect()
+    {
+        return roomRect;
+    }
+
+    public int getDiscovered()
+    {
+        return discovered;
     }
 }
