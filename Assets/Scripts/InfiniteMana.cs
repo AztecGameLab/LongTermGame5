a0def2954869165f882ab9f97ea4119676538863
Joseph Tomada <65485399+j-tomada@users.noreply.github.com>
2021-03-25 21:16:55 -0700

    added comments and descriptions
    Added comments and descriptions to InfiniteMana.cs and ManaPickup.cs

--- Assets/Scripts/InfiniteMana.cs
+++ Assets/Scripts/InfiniteMana.cs
@@ -3,11 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*********************************************************************************************
+ * Script is meant to be applied to a Infinite Mana Spawner
+ * Inherits the entity class in order for the object to appear to take damage
+ * When TakeDamage is called, 1 - 3 mana orbs will spawn which can be picked up by the player
+ * Spawner will always regen back to full health which makes it indestructible.
+*********************************************************************************************/
 public class InfiniteMana : Entity
 {
-    public GameObject manaOrb;
-    private float maxHealth;
-    private float spawnerCordsX;
+    public GameObject manaOrb; //Set this to the Mana Orb prefab
+    private float maxHealth; 
+    private float spawnerCordsX; 
     private float spawnerCordsY;
     private static System.Random rand = new System.Random();
 
@@ -19,7 +25,7 @@ public class InfiniteMana : Entity
         for (int i = 0; i < rand.Next(1, 4); ++i) //Generate 1 - 3 mana orbs
         {
             orbPosition = new Vector2(spawnerCordsX + i + 3, spawnerCordsY); //Makes it so that orbs are separated from each other; Testing purposes
-            Instantiate(manaOrb, orbPosition, Quaternion.identity);
+            Instantiate(manaOrb, orbPosition, Quaternion.identity); //Creates a new Mana Orb prefab on the scene
         }
         base.health = maxHealth; //Health resets back to full 
     }
--- Assets/Scripts/ManaPickup.cs
+++ Assets/Scripts/ManaPickup.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/**************************************************************************************************
+ * Script meant to be added on to the mana orb prefab
+ * Orb will use OnCollisionEnter2D in order to detect when the player collides with the mana orb
+ * On collision, mana orb will be consumed and a set amount of mana will fill up the Fill Bar
+**************************************************************************************************/
 public class ManaPickup : MonoBehaviour
 {
     public float mana; //How much mana each orb will have
@@ -9,9 +14,9 @@ public class ManaPickup : MonoBehaviour
     // Update is called once per frame
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.name == "TempPlayer")
+        if (collision.gameObject.name == "TempPlayer") //If an orb collides with whatever the object of the player's name is
         {
-            ManaSystem.instance.Gain(mana);
+            ManaSystem.instance.Gain(mana); //Calls Gain from FillSystem
             GameObject.Destroy(this.gameObject, 0);
         }
     }
