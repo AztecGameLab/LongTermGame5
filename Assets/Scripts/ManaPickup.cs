57d3f9a4e51a7afcf22cd20f37500be29c1c0da3
Joseph Tomada <65485399+j-tomada@users.noreply.github.com>
2021-03-25 21:10:32 -0700

    Created prefabs and scene
    Created prefabs for both infinite spawner and mana orb
    
    Created a scene that can be used for testing

--- Assets/Scripts/ManaPickup.cs
+++ Assets/Scripts/ManaPickup.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ManaPickup : ManaSystem
+public class ManaPickup : MonoBehaviour
 {
     public float mana; //How much mana each orb will have
 
     // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.name == "TempPlayer")
+        {
+            ManaSystem.instance.Gain(mana);
+            GameObject.Destroy(this.gameObject, 0);
+        }
     }
 }
