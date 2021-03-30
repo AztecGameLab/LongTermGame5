a745da6e345833e550bc327231a6d0427b40d711
poetahto <walls.dk@gmail.com>
2021-03-29 21:24:24 -0700

    added some more documentation to TestAbility class

--- Assets/Scripts/TestAbility.cs
+++ Assets/Scripts/TestAbility.cs
@@ -2,7 +2,11 @@ using UnityEngine.InputSystem;
 
 public class TestAbility : Ability
 {
+    // This is the name of the input which triggers the ability.
+    // You can find the names of all the possible inputs in the GameInputs.inputactions file
+    // (At the time of writing, GameInputs.inputactions is located under Scripts/BaseCode) 
     protected override string InputName => "Move";
+    
     private int _counter = 0;
     
     protected override void Started(InputAction.CallbackContext context)
