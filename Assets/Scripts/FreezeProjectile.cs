ce94bda9913984732a7839fa56fe4c4c4b7db0f2
Andres Maturin <amaturin8749@sdsu.edu>
2021-04-02 12:03:34 -0700

    Created a freeze projectile script that shoots an ice ball. Also added a prefab ice ball, and created a scripts for the ice ball.
    The freeze projectile script was created based of the fireballstats script. As of now the ice ball can only be fired to the right, and the ice ball should freeze the position of a gameobject on impact.

---
+++ Assets/Scripts/FreezeProjectile.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FreezeProjectile : MonoBehaviour
+{
+    public GameObject iceBall;
+    GameObject newIceBall;
+    public Transform shootingPosition;
+    [SerializeField]
+    private float iceBallSpeed;
+    [SerializeField]
+    public float chargeTimer;
+    [SerializeField]
+    private float IceBallSize = 5f;
+
+    public void Fire()
+    {
+        newIceBall = Instantiate(iceBall, shootingPosition.position, shootingPosition.rotation); //creates an iceball
+        IceBallSize += chargeTimer; //the longer the iceball is charged the bigger it will get
+        newIceBall.transform.localScale = new Vector3(IceBallSize, IceBallSize, IceBallSize); //
+        newIceBall.GetComponent<Rigidbody2D>().velocity = (newIceBall.transform.right * iceBallSpeed);
+        chargeTimer = 0f; // resets the timer
+        IceBallSize = 5f; // resets the size of the iceball
+    }
+    public void Charge()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Fire();
+        }
+        
+    }
+}
---
+++ Assets/Scripts/IceBall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceBall : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag != "Player") //incase the spawn position of the ice ball overlaps with the player
+        {
+            Vector3 freeze = collision.gameObject.transform.position; // gets the position of the object it hit
+            collision.gameObject.transform.position = freeze; // "freezes" the gameobject in the position they were hit
+
+            Destroy(gameObject);
+        }
+    }
+}
