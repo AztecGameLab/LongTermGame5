7a37ca9d1a9837ee98f66dafa72ec7d4f7c06402
Andres Maturin <amaturin8749@sdsu.edu>
2021-04-08 15:53:56 -0700

    Changed from MonoBehavior to extend ProjectileWeapon

--- Assets/Scripts/FreezeProjectile.cs
+++ Assets/Scripts/FreezeProjectile.cs
@@ -3,38 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class FreezeProjectile : MonoBehaviour
+public class FreezeProjectile : ProjectileWeapon
 {
     public GameObject iceBall;
     GameObject newIceBall;
     public Transform shootingPosition;
+
     [SerializeField]
     private float iceBallSpeed;
     [SerializeField]
     public float chargeTimer;
     [SerializeField]
-    private float IceBallSize = 5f;
-
+    private float IceBallSize;
+    [SerializeField]
+    private float upForce;
+    private float initialIceBallSize;
+    override
     public void Fire()
     {
         newIceBall = Instantiate(iceBall, shootingPosition.position, shootingPosition.rotation); //creates an iceball
+        initialIceBallSize = IceBallSize;
         IceBallSize += chargeTimer; //the longer the iceball is charged the bigger it will get
         newIceBall.transform.localScale = new Vector3(IceBallSize, IceBallSize, IceBallSize); //
-        newIceBall.GetComponent<Rigidbody2D>().velocity = (newIceBall.transform.right * iceBallSpeed);
+        newIceBall.GetComponent<Rigidbody2D>().velocity = (newIceBall.transform.right * iceBallSpeed) + (newIceBall.transform.up * upForce);
         chargeTimer = 0f; // resets the timer
-        IceBallSize = 5f; // resets the size of the iceball
+        IceBallSize = initialIceBallSize; // resets the size of the iceball
     }
+    override
     public void Charge()
     {
+        if (chargeTimer < 2)
+        {
+            chargeTimer += Time.deltaTime;
 
+        }
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Fire();
-        }
+      //  if (Input.GetMouseButtonDown(0))
+      //  {
+       //     Fire();
+      //  }
         
     }
 }
