dac951bf5409d97b2f3ed13238bcb37e6a002547
Brandon Altamirano <manabran501@hotmail.com>
2021-04-09 13:26:55 -0700

    Implementing force to explosion
    Basic explosion, no final force yet

--- Assets/FireBallCollider.cs
+++ Assets/FireBallCollider.cs
@@ -18,6 +18,16 @@ public class FireBallCollider : MonoBehaviour
         {
             col.gameObject.GetComponent<Entity>().TakeDamage(this.stats.damage);
         }
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(fireball.transform.position, fireball.GetComponent<CircleCollider2D>().radius*5);
+        for(int i=0; i<colliders.Length; ++i)
+        {
+            
+            if(colliders[i].gameObject.GetComponent<Rigidbody2D>() != null)
+            {
+                Debug.Log(colliders[i].gameObject.name);
+                colliders[i].attachedRigidbody.AddForce(colliders[i].gameObject.transform.up * 10, ForceMode2D.Impulse);
+            }
+        }
         Destroy(fireball);
     }
 }
--- Assets/Scripts/FireBallStats.cs
+++ Assets/Scripts/FireBallStats.cs
@@ -12,20 +12,23 @@ public class FireBallStats : ProjectileWeapon
     public float recoil;
     public float upForce;
     public float chargeTimer;
-    public int damage;
+    public float damage;
     public float FireBallSize = 3f;
+    public Vector2 getDir;
 
     private Vector2 radius;
     private bool charging;
 
     public override void Fire(Vector2 direction)
     {
+        getDir = direction;
         charging = false;
         PlatformerController.instance.StopCoroutine(Power());
         chargeTimer = 0f;
         launchForce = 10 + FireBallSize;
         newFireBall.GetComponent<Rigidbody2D>().gravityScale = 3f;
         newFireBall.GetComponent<Rigidbody2D>().velocity = launchForce * direction;
+        newFireBall.GetComponent<Collider2D>().enabled = true;
         FireBallSize = 3f;
     }
 
@@ -33,7 +36,7 @@ public class FireBallStats : ProjectileWeapon
     {
         chargeTimer = 0f;
         damage = 0;
-        fireBall.transform.position = PlatformerController.instance.transform.position + (Vector3)direction;
+        fireBall.transform.position = PlatformerController.instance.transform.position + ((Vector3)direction*2);
         fireBall.transform.position *= PlatformerController.instance.coll.size;
         newFireBall = Instantiate(fireBall, fireBall.transform.position, Quaternion.identity);
         newFireBall.GetComponent<Collider2D>().enabled = false;
@@ -49,14 +52,14 @@ public class FireBallStats : ProjectileWeapon
             chargeTimer += Time.fixedDeltaTime;
             FireBallSize = 3 + chargeTimer;
             newFireBall.transform.localScale = new Vector3(FireBallSize, FireBallSize, FireBallSize);
-            damage += 1;
+            damage += 0.5f;
             yield return null;
         }
         yield return new WaitForSeconds(0f);
     }
     
     public override void OnAimChange(Vector2 direction) {
-        newFireBall.transform.position = PlatformerController.instance.transform.position + (Vector3)direction;
+        newFireBall.transform.position = PlatformerController.instance.transform.position + ((Vector3)direction*2);
         newFireBall.transform.position *= PlatformerController.instance.coll.size;
     }
 }
