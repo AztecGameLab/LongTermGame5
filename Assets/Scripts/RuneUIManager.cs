50242974e1eb6912a89a81085c79809780c0d0f1
EdenArts <ojedaeden08@gmail.com>
2021-04-22 12:34:05 -0700

    Made the Singleton Script
    The singleton script has an on and off function for every rune and has serialized fields so you can drag the on and off material as well as the Images that will be used for the runes

---
+++ Assets/Scripts/RuneUIManager.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RuneUIManager : MonoBehaviour
+{
+    public static RuneUIManager _instance;
+    [SerializeField]
+    Material runeOffMaterial;
+    [SerializeField]
+    Material runeOnMaterial;
+    
+    [SerializeField]
+    GameObject Rune1, Rune2, Rune3, Rune4, Rune5, Rune6, Rune7, Rune8, Rune9;
+    
+
+    public static RuneUIManager instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<RuneUIManager>();
+            }
+            return _instance;
+        }
+    }
+
+    public void turnOffRune1()
+    {
+
+        Rune1.GetComponent<Image>().material = runeOffMaterial;
+        
+    }
+
+    public void turnOnRune1()
+    {
+        Rune1.GetComponent<Image>().material = runeOnMaterial;
+    }
+
+    public void turnOffRune2()
+    {
+
+        Rune2.GetComponent<Image>().material = runeOffMaterial;
+
+    }
+
+    public void turnOnRune2()
+    {
+        Rune2.GetComponent<Image>().material = runeOnMaterial;
+    }
+
+    public void turnOffRune3()
+    {
+
+        Rune3.GetComponent<Image>().material = runeOffMaterial;
+
+    }
+
+    public void turnOnRune3()
+    {
+        Rune3.GetComponent<Image>().material = runeOnMaterial;
+    }
+    public void turnOffRune4()
+    {
+
+        Rune4.GetComponent<Image>().material = runeOffMaterial;
+
+    }
+
+    public void turnOnRune4()
+    {
+        Rune4.GetComponent<Image>().material = runeOnMaterial;
+    }
+
+    public void turnOffRune5()
+    {
+
+        Rune5.GetComponent<Image>().material = runeOffMaterial;
+
+    }
+
+    public void turnOnRune5()
+    {
+        Rune5.GetComponent<Image>().material = runeOnMaterial;
+    }
+
+    public void turnOffRune6()
+    {
+
+        Rune6.GetComponent<Image>().material = runeOffMaterial;
+
+    }
+
+    public void turnOnRune6()
+    {
+        Rune6.GetComponent<Image>().material = runeOnMaterial;
+    }
+
+    public void turnOffRune7()
+    {
+
+        Rune7.GetComponent<Image>().material = runeOffMaterial;
+
+    }
+
+    public void turnOnRune7()
+    {
+        Rune7.GetComponent<Image>().material = runeOnMaterial;
+    }
+
+    public void turnOffRune8()
+    {
+
+        Rune8.GetComponent<Image>().material = runeOffMaterial;
+
+    }
+
+    public void turnOnRune8()
+    {
+        Rune8.GetComponent<Image>().material = runeOnMaterial;
+    }
+
+    public void turnOffRune9()
+    {
+
+        Rune9.GetComponent<Image>().material = runeOffMaterial;
+
+    }
+
+    public void turnOnRune9()
+    {
+        Rune9.GetComponent<Image>().material = runeOnMaterial;
+    }
+}
