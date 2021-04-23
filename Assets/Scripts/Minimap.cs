d16a06a9d8a1abe161f03a42468b95ce31b68d4b
James Maron <71116099+chmilk2@users.noreply.github.com>
2021-04-22 23:46:55 -0700

    Minimap Assets

--- Assets/Scripts/Minimap.cs
+++ Assets/Scripts/Minimap.cs
@@ -3,19 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Minimap : MonoBehaviour
+public class Minimap : Singleton<Minimap>
 {
     public CanvasGroup canvasGroup;
     private Vector2 center = new Vector2(150, 150);
 
-    private static List<Room> rooms;
     private static Texture2D backgroundTexture;
     private static GUIStyle textureStyle;
 
     // Start is called before the first frame update
     void Start()
     {
-        rooms = new List<Room>();
         getRooms();
         backgroundTexture = Texture2D.whiteTexture;
         textureStyle = new GUIStyle { normal = new GUIStyleState { background = backgroundTexture } };
@@ -28,57 +26,13 @@ public class Minimap : MonoBehaviour
         //TODO: Get rooms from bounding boxes
     }
 
-    private void OnGUI()
+    public void toggleMinimap(bool isOn)
     {
-        foreach (Room r in rooms)
-        {
-            if (r.getDiscovered() >= 0)
-            {
-                DrawRect(r.getRect(center), (r.getDiscovered() == 1 ? Color.white : Color.yellow));
-            }
-            
-        }
-        
-    }
 
-    public static void DrawRect(Rect position, Color color, GUIContent content = null)
-    {
-        var backgroundColor = GUI.backgroundColor;
-        GUI.backgroundColor = color;
-        GUI.Box(position, content ?? GUIContent.none, textureStyle);
-        GUI.backgroundColor = backgroundColor;
     }
-
     // Update is called once per frame
     void Update()
     {
         
-    }
-}
-
-//rect wrapper object to hold rectangles of all the rooms that can be shifed by a center value
-class Room
-{
-    Rect roomRect;
-    int discovered = -1;
-
-    public Room(Rect rect, int discovered = -1)
-    {
-        this.roomRect = rect;
-        this.discovered = discovered;
-    }
-    public Rect getRect()
-    {
-        return roomRect;
-    }
-
-    public Rect getRect(Vector2 center)
-    {
-        return new Rect(roomRect.x+center.x, roomRect.y+center.y, roomRect.width,roomRect.height);
-    }
-
-    public int getDiscovered()
-    {
-        return discovered;
     }
 }
