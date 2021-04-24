0c0e0375a8c74823a31a77af9b1cdd10bad1da41
Kain Shepard <kainkun100@gmail.com>
2021-04-24 15:53:06 -0700

    ice

--- Assets/Scripts/Freeze.cs
+++ Assets/Scripts/Freeze.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Freeze : MonoBehaviour
@@ -10,7 +11,13 @@ public class Freeze : MonoBehaviour
         Collider2D collider = GetComponent<Collider2D>();
         Debug.Log("Made it");
         collider.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+        var sr = GetComponentInChildren<SpriteRenderer>();
+        var go = Instantiate(Resources.Load("Ice") as GameObject, sr.bounds.center, quaternion.identity);
+        go.transform.localScale = Vector3.one * sr.bounds.size.y;
+        
         yield return new WaitForSeconds(5.0f);
+        
+        Destroy(go);
         collider.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
         Debug.Log("unfreeze");
         Destroy(this);
