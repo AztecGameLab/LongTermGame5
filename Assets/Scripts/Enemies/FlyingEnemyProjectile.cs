66dd77306b36f6bc1576b927c2eaf96fc77c919b
poetahto <walls.dk@gmail.com>
2021-04-24 15:49:48 -0700

    flying enemy projectiles now face the direction they move

--- Assets/Scripts/Enemies/FlyingEnemyProjectile.cs
+++ Assets/Scripts/Enemies/FlyingEnemyProjectile.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,11 +16,17 @@ public class FlyingEnemyProjectile : MonoBehaviour
         player = PlatformerController.instance;
         rb = GetComponent<Rigidbody2D>();
         rb.AddForce((player.transform.position - transform.position).normalized * speed);
+
         //destroy itself after 5 seconds
         StartCoroutine(DestroyProjectile());
     }
 
-IEnumerator DestroyProjectile()
+    private void Update()
+    {
+        transform.right = rb.velocity;
+    }
+
+    IEnumerator DestroyProjectile()
     {
         yield return new WaitForSeconds(5f);
         Destroy(gameObject);
