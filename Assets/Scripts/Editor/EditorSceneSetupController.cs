bd386dd0566f66ab3b06e4943935e5ff6b322345
poetahto <walls.dk@gmail.com>
2021-04-24 18:04:51 -0700

    integrated editor with gameplay events, fixing timing bug

--- Assets/Scripts/Editor/EditorSceneSetupController.cs
+++ Assets/Scripts/Editor/EditorSceneSetupController.cs
@@ -1,4 +1,6 @@
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using static Editor.EditorUtil;
 
 namespace Editor
@@ -6,12 +8,21 @@ namespace Editor
     public static class EditorSceneSetupController
     {
         [RuntimeInitializeOnLoadMethod]
-        private static void LoadDependentScenes()
+        private static async void LoadDependentScenes()
         {
             EnsureSceneIsLoaded("Persistent");
 
             if (HasPlayerSpawn(out var playerSpawn))
                 CreatePlayerAt(playerSpawn.transform.position);
+            
+            await Task.Yield();
+            
+            var currentLevel = LevelController.Get().GetLevel(SceneManager.GetActiveScene().name);
+            
+            if (currentLevel.isGameplayLevel)
+                GameplayEventChannel.PublishStart();
+            else
+                GameplayEventChannel.PublishEnd();  
         }
 
         private static void CreatePlayerAt(Vector3 position)
