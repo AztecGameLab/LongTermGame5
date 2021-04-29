d7af26ef8e0c394ccb12d4bb2d28feceeeafcb5f
Kain Shepard <kainkun100@gmail.com>
2021-04-29 12:16:01 -0700

    tweaks

---
+++ Assets/LavaFlies.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using FMODUnity;
+
+
+public class LavaFlies : MonoBehaviour
+{
+    public float interval = 1.5f;
+    public float damagePerTick = 1;
+    private bool playerInTrigger;
+
+    [EventRef] public string damageSound;
+    [EventRef] public string angrySound;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var pc = other.GetComponent<PlatformerController>();
+        if (pc)
+        {
+            playerInTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        var pc = other.GetComponent<PlatformerController>();
+        if (pc)
+        {
+            playerInTrigger = false;
+        }
+    }
+
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            while (playerInTrigger)
+            {
+                PlatformerController.instance.TakeDamage(damagePerTick);
+                print("tick");
+                RuntimeManager.PlayOneShot(damageSound, transform.position);
+                yield return new WaitForSeconds(interval);
+                yield return null;
+            }
+            yield return null;
+        }
+    }
+}
