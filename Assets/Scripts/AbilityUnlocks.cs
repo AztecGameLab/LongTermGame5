7667528ae0ec5010556e8489153a55bdb8aa1cae
Kain Shepard <kainkun100@gmail.com>
2021-04-30 00:20:09 -0700

    ability unlocks

--- Assets/Scripts/AbilityUnlocks.cs
+++ Assets/Scripts/AbilityUnlocks.cs
@@ -11,9 +11,12 @@ public class AbilityUnlocks : Singleton<AbilityUnlocks>
     
     public void Unlock(Abilities ability)
     {
+        var pc = GetComponent<PlatformerController>();
+        
         print(ability);
         switch (ability)
         {
+
             case Abilities.Slide:
                 print("addslide");
                 gameObject.AddComponent<SlideAbility>();
@@ -21,7 +24,6 @@ public class AbilityUnlocks : Singleton<AbilityUnlocks>
                 break;
             case Abilities.ReflectingProjectile:
                 print("addrico");
-                var pc = GetComponent<PlatformerController>();
                 pc.weapons.Add(Resources.Load<Ricochet>("Weapons/Ricochet"));
                 pc.currWeapon = pc.weapons.Count - 1;
                 PlatformerController.instance.currentUnlockState = 2;
@@ -33,6 +35,8 @@ public class AbilityUnlocks : Singleton<AbilityUnlocks>
                 break;
             case Abilities.FireBall:
                 print("unlock");
+                pc.weapons.Add(Resources.Load<Ricochet>("Weapons/FireBall"));
+                pc.currWeapon = pc.weapons.Count - 1;
                 PlatformerController.instance.currentUnlockState = 4;
                 break;
             case Abilities.DoubleJump:
@@ -42,6 +46,8 @@ public class AbilityUnlocks : Singleton<AbilityUnlocks>
                 break;
             case Abilities.FreezeProjectile:
                 print("unlock");
+                pc.weapons.Add(Resources.Load<Ricochet>("Weapons/FreezeProjectile"));
+                pc.currWeapon = pc.weapons.Count - 1;
                 PlatformerController.instance.currentUnlockState = 6;
                 break;
             case Abilities.GroundPound:
@@ -50,6 +56,8 @@ public class AbilityUnlocks : Singleton<AbilityUnlocks>
                 break;
             case Abilities.Grapple:
                 print("unlock");
+                pc.weapons.Add(Resources.Load<Ricochet>("Weapons/GrappleHook"));
+                pc.currWeapon = pc.weapons.Count - 1;
                 PlatformerController.instance.currentUnlockState = 8;
                 break;
             case Abilities.None:
